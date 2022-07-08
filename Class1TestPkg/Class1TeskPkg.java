cce0c771d0d0f4cd82c6421d2d086d93651b3796
Gartoj <106785023+Gartoj@users.noreply.github.com>
2022-07-07 22:09:00 -0400

    Update Class1TeskPkg.java

--- Class1TestPkg/Class1TeskPkg.java
+++ Class1TestPkg/Class1TeskPkg.java
@@ -4,5 +4,6 @@ public class Class1TeskPkg {
     public static void main(String[] args) {
         System.out.println("Study hall git stTest Practice!");
         System.out.println("Testing Fetch Branch");
+        System.out.println("Testing Fetch Branch");
     }
 }
