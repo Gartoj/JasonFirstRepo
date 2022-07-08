694ebe8a6e0705749e100db21b3e0e81e18837df
gartoj <gartoj@live.com>
2022-07-07 22:05:18 -0400

    Fetch

--- Class1TestPkg/Class1TeskPkg.java
+++ Class1TestPkg/Class1TeskPkg.java
@@ -3,5 +3,6 @@ package Class1TestPkg;
 public class Class1TeskPkg {
     public static void main(String[] args) {
         System.out.println("Study hall git stTest Practice!");
+        System.out.println("Testing Fetch Branch");
     }
 }
