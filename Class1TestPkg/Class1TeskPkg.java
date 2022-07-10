7e38d3a6111529027a9944653348899b3235c9e6
Gartoj <106785023+Gartoj@users.noreply.github.com>
2022-07-10 17:48:21 -0400

    Update Class1TeskPkg.java

--- Class1TestPkg/Class1TeskPkg.java
+++ Class1TestPkg/Class1TeskPkg.java
@@ -10,5 +10,6 @@ public class Class1TeskPkg {
         System.out.println("Pizza Test");
         System.out.println("Kerim Test Push");
         System.out.println("PULL OR FETCH");
+        System.out.println("ManagerTest");
     }
 }
